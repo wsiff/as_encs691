57067de1c4bec1af1816cae88172538065597421
Sif <43895028+wsiff@users.noreply.github.com>
2024-12-11 16:44:38 -0500

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN_AUCTION_PLATFORM",
+      dbName: "AUCTION_SYSTEM",
     })
     .then(() => {
       console.log("Connected to database.");
